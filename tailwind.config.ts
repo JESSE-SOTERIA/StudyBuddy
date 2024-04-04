dd9945da624e93d4dd2d7c362c11f67c130419e7
JESSE-SOTERIA <111914569+JESSE-SOTERIA@users.noreply.github.com>
2024-04-04 21:58:02 +0300

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: '#000000',
+        customGreen: '#10D570',
+        customWhite: '#FFFFFF',
+        customDarkGreen: '#025908',
+        customLightGreen: '#04D912',
+        customDark: '#1C2D3D'
+      }
+    }
+  },
+  plugins: [],
+}
+export default config
